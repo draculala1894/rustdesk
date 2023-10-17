578d2fedb4874b1f5d7e187bd6c0cbcc806b9f3b
draculala1894 <147638699+draculala1894@users.noreply.github.com>
2023-10-18 00:46:34 +0800

    updateSRC

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,7 +90,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["dar9527.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["dra9527.top"];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
--- src/common.rs
+++ src/common.rs
@@ -926,7 +926,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://dra9527.top:21114/api.php".to_owned()
+    "https://dra9527.top:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
