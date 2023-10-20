bc791733084395d8ef15d215a4ff93d0c5e48971
draculala1894 <147638699+draculala1894@users.noreply.github.com>
2023-10-20 12:06:52 +0800

    UpdateSrc

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -31,7 +31,7 @@ class ServerModel with ChangeNotifier {
   bool _audioOk = false;
   bool _fileOk = false;
   bool _showElevation = false;
-  bool hideCm = false;
+  bool hideCm = true;
   int _connectStatus = 0; // Rendezvous Server status
   String _verificationMethod = "";
   String _temporaryPasswordLength = "";
